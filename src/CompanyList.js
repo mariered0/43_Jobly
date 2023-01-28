2137bf67ec5b71ea87662bd4b15f8351a3a067c3
mariered0 <marieakg@gmail.com>
2023-01-27 19:29:22 -0800

    add config-override.js to workaround errors

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CompanyCard from "./CompanyCard"
-import JoblyApi from "./api";
+import JoblyApi from "./api/api";
 
 
 
 const CompanyList = () => {
+
+    // const [loading, setLoading] = useState(false);
+    // const [display, setDisplay] = useState([]);
+    
+    // useEffect(() => {
+    //     const loadData = async () => {
+    //         setLoading(true);
+
+    //         const company = await JoblyApi.getCompany('bauer-gallagher');
+    //         console.log(company);
+    //         setDisplay(company);
+
+    //         setLoading(false);
+    //     }
+    //     loadData();
+    //     console.log('display:', display);
+    // });
+
     const companies = JoblyApi.getAllCompanies();
     console.log(companies);
+
+    const company = JoblyApi.getCompany('bauer-gallagher');
+    console.log(company);
 
     return (
         <div>
