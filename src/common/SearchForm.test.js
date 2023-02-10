18cac49ca863f076766dd1234aa9a543f1579b67
mariered0 <marieakg@gmail.com>
2023-02-09 20:02:13 -0800

    add tests

---
+++ src/common/SearchForm.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import SearchForm from "./SearchForm";
+
+// it("renders without crashing", () => {
+//     render(<SearchForm />);
+// });
+
+// it("matches snapshot", () => {
+// 	const { asFragment } = render(<SearchForm />);
+// 	expect(asFragment()).toMatchSnapshot();
+// });
